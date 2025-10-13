68b650fffed7a69ec3a16945e6b62b4d8dc55f9e
AnjanaUnnikrishnan24 <anjana24unni@gmail.com>
2025-10-13 21:51:12 +0530

    update

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaWhatsapp } from "react-icons/fa";
 import authImage from '../assets/authImage.png';
 
 const HeroSection = () => {
@@ -10,21 +9,29 @@ const HeroSection = () => {
         backgroundImage: `url(${authImage})`,
       }}
     >
+      <style>
+        {`@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+        .pacifico-regular {
+          font-family: "Pacifico", cursive;
+          font-weight: 400;
+          font-style: normal;
+        }`}
+      </style>
 
       {/* Hero Content */}
-      <div className="relative flex flex-col items-center justify-center text-center flex-1 px-6 mt-24">
-        <h3 className="text-sm md:text-lg font-medium uppercase mb-3 tracking-wide text-black">
+      <div className="relative flex flex-col flex-1 px-6 mt-[100px] ml-[80px]">
+        <h3 className="text-sm md:text-lg font-medium uppercase mb-3 ml-[240px] tracking-wide text-black">
           Roofing Services in Your City
         </h3>
-        <h1 className="text-4xl md:text-6xl font-extrabold mb-4 text-black">
+        <h1 className="text-4xl md:text-6xl font-extrabold mb-4 text-black pacifico-regular ml-[20px]">
           DuroCap Roofing Solutions
         </h1>
-        <p className="max-w-2xl text-black font-extrabold text-base md:text-lg mb-8">
+        <p className="max-w-2xl text-black font-extrabold text-center text-base md:text-lg mb-8 ml-[70px]">
           We are DuroCap — offering trusted, durable, and affordable roofing
           solutions for commercial and residential projects across the region.
         </p>
 
-        <div className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col sm:flex-row gap-4 ml-[200px]">
           <button className="bg-blue-600 hover:bg-white hover:text-black text-white px-6 py-3 rounded-lg font-semibold transition">
             View Our Services
           </button>
--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -5,8 +5,8 @@ import HeroSection from '../components/HeroSection';
 import FeatureCards from '../components/FeatureCards';
 import StatsBanner from '../components/StatsBanner';
 import Services from '../components/Services';
-import { Phone } from 'lucide-react'; 
 import CategorySection from '../components/CategorySection';
+import Estimate from '../components/Estimate';
 
 const Homepage = () => {
   return (
@@ -16,42 +16,7 @@ const Homepage = () => {
       <FeatureCards />
       <CategorySection />
       <Services />
-
-      {/* Need a Free Estimate Section */}
-      <section className="bg-white py-16 px-6 md:px-20 flex flex-col md:flex-row items-center justify-between">
-        {/* Left Content */}
-        <div className="max-w-xl text-center md:text-left">
-          <h2 className="text-3xl md:text-4xl font-bold text-[#1e2a78] mb-4">
-            Need A Free Estimate?
-          </h2>
-          <p className="text-gray-700 leading-relaxed mb-6">
-            If you want a roofing installation or replacement done or know someone
-            who needs a roof repair and coating, get in touch with us today! Get
-            the Best Roofing Services in Trivandrum from AJ Roofings.
-          </p>
-
-          {/* Buttons */}
-          <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
-            <button className="bg-[#1e2a78] text-white px-6 py-3 rounded-md font-medium hover:bg-[#2938a0] transition">
-              GET A FREE ESTIMATE
-            </button>
-
-            <button className="bg-[#1e2a78]/90 text-white px-6 py-3 rounded-md font-medium flex items-center justify-center gap-2 hover:bg-[#2938a0] transition">
-              <Phone size={18} /> CALL US NOW
-            </button>
-          </div>
-        </div>
-
-        {/* Right Illustration */}
-        {/* <div className="mt-10 md:mt-0 md:ml-10">
-          <img
-            src={}
-            alt="Customer support illustration"
-            className="max-w-sm w-full"
-          />
-        </div> */}
-      </section>
-
+      <Estimate />
       <StatsBanner />
       <Footer />
     </>
